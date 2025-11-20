be672224cb90b4dbf871aff873ef264abf7d570d
ankit kumar <55060673+ankitkumar0@users.noreply.github.com>
2025-11-20 23:09:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 
 // Hostinger MySQL Credentials (इन्हें अपनी Hostinger जानकारी से बदलें!)
 const db = mysql.createPool({
-    host: 'localhost', // आमतौर पर 'localhost' या Hostinger का IP
-    user: 'u173296742_aviator', 
-    password: 'akgSG@03091995',
-    database: 'u173296742_aviator',
+    host: process.env.DB_HOST, // आमतौर पर 'localhost' या Hostinger का IP
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
     waitForConnections: true,
     connectionLimit: 10
 });
